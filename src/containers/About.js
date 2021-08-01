3de5ea4f2f453ae60b6c9f5c05cbd1f338fbd3b4
KerronKing <king_of_palmiste@hotmail.com>
2021-08-01 14:00:17 -0400

    progress commit 7 - about/1st update

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import "./About.scss";
 
 const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
+  <>
+    <Navbar />
+
+    {/* Banner */}
+
+    <div className="about-us-first-section">
+      <div className="homepage-link">
+        <Link to="/">&lt;&lt; Back to Homepage</Link>
+      </div>
+
+      <div className="about-us-banner">
+        <h1>About I Dance Academy</h1>
+      </div>
+    </div>
+
+    {/* Founder Section */}
+
+    <div className="about-us-founder-section">
+      <img src="" alt="" />
+      <h2>Our Mission</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        blandit varius elit, nec dignissim massa elementum quis. Nam turpis
+        nibh, accumsan non nisi et, volutpat interdum neque. Proin mollis lorem
+        eu nisl pulvinar, non porta dolor sollicitudin. Donec cursus rutrum
+        tortor eu imperdiet. Sed cursus ligula venenatis sollicitudin
+        vestibulum.
+      </p>
+      <h2>History</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        blandit varius elit, nec dignissim massa elementum quis. Nam turpis
+        nibh, accumsan non nisi et, volutpat interdum neque. Proin mollis lorem
+        eu nisl pulvinar, non porta dolor sollicitudin. Donec cursus rutrum
+        tortor eu imperdiet. Sed cursus ligula venenatis sollicitudin
+        vestibulum.
+      </p>
+      <h2>Founder</h2>
+      <img src="" alt="" />
+      <p className="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+    </div>
+
+    <Footer />
+  </>
 );
 
 export default About;
