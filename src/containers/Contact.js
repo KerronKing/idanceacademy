708dd07d18230232bdc6ce115135af3d782f16f4
KerronKing <king_of_palmiste@hotmail.com>
2021-08-02 13:33:35 -0400

    progress commit 8 - contact page design/first draft complete

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React from "react";
+import Footer from "../components/Footer";
+import "./Contact.scss";
 
-const Contact = () => (
-  <div>
-    Contact
-  </div>
-);
+const Contact = () => {
+  return (
+    <>
+      <div className="contact-main-section">
+        <div className="d-flex flex-row justify-content-center contact-inner">
+          <div className="contact-1">
+            <div className="contact-info">
+              <div className="contact-item">
+                <h3>Visit us at:</h3>
+                <p>#142 Coora Road, Siparia</p>
+              </div>
+              <div className="contact-item">
+                <h3>Call us:</h3>
+                <p>1-868-712-8342 / 1-868-396-1866</p>
+              </div>
+              <div className="contact-item">
+                <h3>Email us:</h3>
+                <p>idanceacademy868@gmail.com</p>
+              </div>
+            </div>
+          </div>
+          <form className="contact-2">
+            <div class="row">
+              <div class="col-md">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="First name"
+                />
+              </div>
+              <div class="col-md">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Last name"
+                />
+              </div>
+            </div>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Email address"
+            />
+            <div class="form-group">
+              <input
+                type="text"
+                class="form-control"
+                id="subject"
+                aria-describedby="subject"
+                placeholder="Message subject"
+              />
+            </div>
+            <div class="form-group">
+              <textarea
+                class="form-control"
+                id="message"
+                rows="8"
+                placeholder="Enter your message"
+              ></textarea>
+            </div>
+            <input
+              type="submit"
+              name="submit"
+              id="submit-message"
+              placeholder="submit"
+            />
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Contact;
