1d6ec5e08a49fa31067d0274fde205dcf59c4a9d
KerronKing <king_of_palmiste@hotmail.com>
2021-08-05 23:36:43 -0400

    progress commit 11 - classes/services pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,10 +4,17 @@ import Homepage from "../containers/Homepage";
 import About from "../containers/About";
 import Contact from "../containers/Contact";
 import Registration from "../containers/Registration";
-import Services from "../containers/Services";
+import Services from "../services/Services";
 import Schedule from "../containers/Schedule";
 import Events from "../containers/Events";
 import Gallery from "../containers/Gallery";
+import BeginnerBallet from "../services/BeginnerBallet";
+import YouthBallet from "../services/YouthBallet";
+import ModernDance from "../services/ModernDance";
+import Jazz from "../services/Jazz";
+import MarriagePackges from "../services/MarriagePackages";
+import GraduationPackages from "../services/GraduationPackages";
+import MiscPackages from "../services/MiscPackages";
 
 const App = () => {
   return (
@@ -21,6 +28,13 @@ const App = () => {
         <Route path="/schedule" component={Schedule} />
         <Route path="/events" component={Events} />
         <Route path="/gallery" component={Gallery} />
+        <Route path="/beginner-ballet" component={BeginnerBallet} />
+        <Route path="/youth-ballet" component={YouthBallet} />
+        <Route path="/modern-dance" component={ModernDance} />
+        <Route path="/jazz" component={Jazz} />
+        <Route path="/marriage-packages" component={MarriagePackges} />
+        <Route path="/graduation-packages" component={GraduationPackages} />
+        <Route path="/misc-services" component={MiscPackages} />
       </Switch>
     </div>
   );
---
+++ src/services/MarriagePackages.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./Services.scss";
+
+const MarriagePackages = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="class">
+        <img src="" alt="" />
+        <h1>Marriage Packages</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          blandit varius elit, nec dignissim massa elementum quis. Nam turpis
+          nibh, accumsan non nisi et, volutpat interdum neque. Proin mollis
+          lorem eu nisl pulvinar, non porta dolor sollicitudin. Donec cursus
+          rutrum tortor eu imperdiet. Sed cursus ligula venenatis sollicitudin
+          vestibulum. In maximus massa purus, sed venenatis arcu varius tempus
+        </p>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
+export default MarriagePackages;
