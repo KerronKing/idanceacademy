4de6495254a69983bbbb2d56aa732d73e6076ef5
KerronKing <king_of_palmiste@hotmail.com>
2021-08-10 17:50:09 -0400

    progress commit 17 - gallery

--- src/containers/Gallery.js
+++ src/containers/Gallery.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ImageGallery from 'react-image-gallery';
-import imageLinks from '../data/imagelinks';
+import React from "react";
+import Carousel from "react-gallery-carousel";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "react-gallery-carousel/dist/index.css";
+import imageLinks from "../data/imagelinks";
+import "./Gallery.scss";
 
 const Gallery = () => {
-  return(
-    <ImageGallery items={imageLinks} />
+  return (
+    <>
+      <Navbar />
+      <div className="gallery">
+        <Carousel images={imageLinks} className="gal-window" />
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default Gallery;
--- src/data/imagelinks.js
+++ src/data/imagelinks.js
@@ -1,16 +1,7 @@
 const imageLinks = [
-  {
-    original: "",
-    thumbnail: "",
-  },
-  {
-    original: "",
-    thumbnail: "",
-  },
-  {
-    original: "",
-    thumbnail: "",
-  },
+  { src: 'https://drive.google.com/uc?export=view&id=1cC_rfY4kilPjLJ8_5wkavrWQS_fLmrij' },
+  { src: 'https://www.dropbox.com/s/8ymfhh6qpgsmr6c/franciszek-augustyniak-82I3FAg4YtU-unsplash.jpg?raw=1' },
+  { src: 'https://www.dropbox.com/s/vpzxbai48v0y073/courtney-r-andrews-u5MXF-5y1Ck-unsplash.jpg?raw=1' }
 ];
 
 export default imageLinks;
