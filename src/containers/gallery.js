9cc92c93634e737305d66e08c439d521a8be82f1
KerronKing <king_of_palmiste@hotmail.com>
2021-08-17 21:49:19 -0400

    progress commit 20 - update gallery

--- src/containers/gallery.js
+++ src/containers/gallery.js
@@ -4,6 +4,9 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "react-gallery-carousel/dist/index.css";
 import imageLinks from "../data/imagelinks";
+// import image1 from "../images/aliaksei-manlyx-YipPfXLCiUo-unsplash.jpg";
+// import image2 from "../images/alireza-dolati-eqLEGlP8tO4-unsplash.jpg";
+// import image3 from "../images/ballet-service-1.jpeg";
 import "./Gallery.scss";
 
 const Gallery = () => {
--- src/data/imagelinks.js
+++ src/data/imagelinks.js
@@ -1,15 +1,15 @@
 const imageLinks = [
-  { src: 'https://drive.google.com/uc?export=view&id=1ekJiR3lhSyZFwXjlEoov4l8ivoyX0pgJ' },
-  { src: 'https://drive.google.com/uc?export=view&id=1MM7A1kypeL8v1dYc2xifexb_DGLnrhcQ' },
-  { src: 'https://drive.google.com/uc?export=view&id=1OjdsFukGKeWZmtI8gOPmPFRZNUDcKDNV' },
-  { src: 'https://drive.google.com/uc?export=view&id=15wDk3MJlA1I68dReBFw45yoXukbTfdLL' },
-  { src: 'https://drive.google.com/uc?export=view&id=1I-nonY3FsQy8VaqP4kvly1P1L0IpJbY7' },
-  { src: 'https://drive.google.com/uc?export=view&id=1-ql1pXXVxeYIOAE-ugieSWJ2SUUV49nZ' },
-  { src: 'https://drive.google.com/uc?export=view&id=1lS3h98sThGM7EyL5-kVBg9eWMvATKxVo' },
-  { src: 'https://drive.google.com/uc?export=view&id=11CQtzskYzkWyc0RIu_nbwcPV4kCXVt05' },
-  { src: 'https://drive.google.com/uc?export=view&id=1yBUTt0eBK5_Hti0vTOzhWL2JVGCtJNIH' },
-  { src: 'https://drive.google.com/uc?export=view&id=1f55ERwktyptiX9B5nPgOqRwEMQHxAEwU' },
-  { src: 'https://drive.google.com/uc?export=view&id=1eDrzzfCX2MZrBff3vUKdsIN0aYs8tIHi' }
+  { src: 'https://www.dropbox.com/s/20yg33o571xzt4e/idg1.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/dtkzgz7x13kd5pf/idg2.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/23t4s8jquvvoqy4/idg3.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/qozodtcdrzdf4fj/idg4.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/8bsn4qvdcyzvj48/idg5.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/8z5dw3dkjnu7ued/idg6.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/il2pywdne7ctocv/idg7.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/riww9p1zu3vh9ge/idg8.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/ouaix4lp0i97agj/idg9.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/6ih6cg4n7a8javj/idg10.jpeg?raw=1' },
+  { src: 'https://www.dropbox.com/s/7hvhfsvts9ver8x/img11.jpeg?raw=1' }
 ];
 
 export default imageLinks;
